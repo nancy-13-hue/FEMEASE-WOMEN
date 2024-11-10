126f89e9b05fb6446c926d56965868bd84f3d4cf
NANCY GUPTA <22501083nancygupta@lb.du.ac.in>
2024-11-10 11:33:25 +0530

    Add files via upload

---
+++ CHATBOT-FOR-WOMEN/PUBLIC/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "OPENAI_API_KEY": "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"
+};
